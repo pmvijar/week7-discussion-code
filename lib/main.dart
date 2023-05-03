a380bbd7f24260636c330c83e2060f62370db540
unknown <cbarilea@up.edu.ph>
2023-05-03 15:30:00 +0800

    done na lab

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,20 @@
   Date: updated April 26, 2023
   Description: Sample todo app with Firebase 
 */
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/todo_provider.dart';
 import 'screens/todo_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MultiProvider(
       providers: [
@@ -34,6 +41,7 @@ class MyApp extends StatelessWidget {
       },
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true
       ),
     );
   }
